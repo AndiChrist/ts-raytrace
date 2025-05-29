bb94afe3baa0773b052f95e063619b8e2b8cd3aa
Andreas Christ <andichrist@gmx.de>
2025-05-29 19:18:05 +0200

    anti-aliasing added

--- src/main.ts
+++ src/main.ts
@@ -24,16 +24,25 @@ const tracer = new RayTracer(scene);
 
 const png = new PNG({ width, height });
 
+const samplesPerPixel = 4;
+
 for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-        const u = x / (width - 1);
-        const v = 1 - y / (height - 1);
-        const ray = camera.getRay(u, v);
-        const color = tracer.trace(ray).toRGB();
+        let finalColor = new Color(0, 0, 0);
+
+        for (let s = 0; s < samplesPerPixel; s++) {
+            const u = (x + Math.random()) / (width - 1);
+            const v = 1 - (y + Math.random()) / (height - 1);
+            const ray = camera.getRay(u, v);
+            finalColor = finalColor.add(tracer.trace(ray));
+        }
+
+        finalColor = finalColor.multiply(1 / samplesPerPixel);
+        const rgb = finalColor.toRGB();
         const idx = (y * width + x) * 4;
-        png.data[idx] = color[0];
-        png.data[idx + 1] = color[1];
-        png.data[idx + 2] = color[2];
+        png.data[idx] = rgb[0];
+        png.data[idx + 1] = rgb[1];
+        png.data[idx + 2] = rgb[2];
         png.data[idx + 3] = 255;
     }
 }
